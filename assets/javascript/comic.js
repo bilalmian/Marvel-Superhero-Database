6ca0c7d125ff68ccc02b4fc39ed77c6149e982ce
Jahdasha Flagg <jahdashaflagg@Jahdashas-MBP.home>
2016-03-27 18:47:59 -0400

    store the refined results in variables in comic.js

--- assets/javascript/comic.js
+++ assets/javascript/comic.js
@@ -16,31 +16,51 @@ $( document ).ready(function() {
 
         $.ajax({url: url, method: "GET"}).done
             (function(data) {
-            // console.log(data);  // console log of full data return
+        // console.log(data);  // console log of full data return
 
-            // drilling down to the needed data and place in a variable
+        // drilling down to the needed data and place in a variable
             var refinedResults = data.data.results[0];
             console.log(refinedResults); // console log of comic result
 
-            // Usefull results from refindedResults
-                // comics : Object
-                // description : "Bitten by a radioactive spider, high school student Peter Parker gained the speed, strength and powers of a spider. Adopting the name Spider-Man, Peter hoped to start a career using his new abilities. Taught that with great power comes great responsibility, Spidey has vowed to use his powers to help people."
-                // events : Object
-                // name : "Spider-Man"
-                // series : Object
-                // stories : Object
+        // Useful results from refinded results
+            // 1. comics --  its an object
+            // 2. description -- description of the comic               
+            // 3. events --  its an object
+            // 4. name -- name of the comic
+            // 5. series --  its an object
+            // 6. stories --  its an object
 
-            // drilling down to the needed data and place in a variable
+        // store the refined results in variables
+            // comics object
             var comicResults = data.data.results[0].comics;
             console.log(comicResults); // console log of comic result
 
-            var jsonText = JSON.stringify(comicResults);
-            $("#comicsView").html(jsonText);
-                         
+            // description item
+            var descriptionResults = data.data.results[0].description;
+            console.log(descriptionResults); // console log of description result
 
+            // events object
+            var eventsResults = data.data.results[0].events;
+            console.log(eventsResults); // console log of events result
+
+            // name item
+            var nameResults = data.data.results[0].name;
+            console.log(nameResults); // console log of description result
+
+            // series object
+            var seriesResults = data.data.results[0].series;
+            console.log(seriesResults); // console log of events result
+
+            // stories object
+            var storiesResults = data.data.results[0].stories;
+            console.log(storiesResults); // console log of events result
+
+            // var jsonText = JSON.stringify(comicResults);
+            // $("#comicsView").html(jsonText);
+                         
          });
 
-        return false; // cancels the input submit button, rather than it going to another page when we right false it does nothing
+        return false; // stop the page from refreshing when the submit button is hit
     });
 });
 
