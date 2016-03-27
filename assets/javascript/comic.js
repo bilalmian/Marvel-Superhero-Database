a1699f91172dcb6c05097064e9b90b51573a7275
Jahdasha Flagg <jahdashaflagg@Jahdashas-MBP.home>
2016-03-27 17:31:03 -0400

    adding marvel call to branch

---
+++ assets/javascript/comic.js
@@ -0,0 +1,29 @@
+$( document ).ready(function() {
+    $(document).on("click", "#heroSubmit", function(){
+        var heroName = $('#heroSearch').val();
+
+        // Variable needed for the hash
+        var ts = new Date().getTime(); 
+        var PRIV_KEY = "9ced83e38521d6850703a1c54a18fea775a60964"
+        var API_KEY = "bdcf4ef10cbcac4120f014ab0e020243";
+        var hash = CryptoJS.MD5(ts+PRIV_KEY + API_KEY);
+        // apikey for the end of the url
+        var apiKey = "bdcf4ef10cbcac4120f014ab0e020243";
+
+        // var url = "http://gateway.marvel.com/v1/public/characters?name=spider-man&ts="+ts+"&hash="+hash+"&apikey="+apiKey;
+        var url = "https://gateway.marvel.com/v1/public/characters?name="+heroName+"&ts="+ts+"&hash="+hash+"&apikey="+apiKey;
+        console.log(url);
+
+        $.ajax({url: url, method: "GET"}).done
+            (function(data) {
+            console.log(data);
+            // console test to make sure something returns
+            var jsonText = JSON.stringify(data);
+            $("#comicsView").html(jsonText);
+                         
+         });
+
+        return false; // cancels the input submit button, rather than it going to another page when we right false it does nothing
+    });
+});
+
