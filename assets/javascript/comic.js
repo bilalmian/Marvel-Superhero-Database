c638f2c5aa3a0dd21db3db15bff997adb7a8fc65
Jahdasha Flagg <jahdashaflagg@Jahdashas-MBP.home>
2016-03-27 20:35:05 -0400

    store the refined results in variables in comic.js

--- assets/javascript/comic.js
+++ assets/javascript/comic.js
@@ -2,45 +2,46 @@ $( document ).ready(function() {
     $(document).on("click", "#heroSubmit", function(){
         var heroName = $('#heroSearch').val();
 
-        // Variables needed for the hash
+    // Variables needed for the hash
         var ts = new Date().getTime(); 
         var PRIV_KEY = "9ced83e38521d6850703a1c54a18fea775a60964"
         var API_KEY = "bdcf4ef10cbcac4120f014ab0e020243";
         var hash = CryptoJS.MD5(ts+PRIV_KEY + API_KEY);
-        // apikey for the end of the url
+    // apikey for the end of the url
         var apiKey = "bdcf4ef10cbcac4120f014ab0e020243";
 
+    // url to pass through the ajax call
         var url = "https://gateway.marvel.com/v1/public/characters?name=" + heroName + "&ts=" + ts + "&hash=" + hash + "&apikey=" + apiKey;
         console.log(url); // console log of url
 
-        $.ajax({
-                url: url, 
-                method: "GET"
-            })
-            .done(function(data) {
+    // ajax call
+        $.ajax({ url: url, method: "GET" }).done(function(data) {
         // console.log(data);  // console log of full data return
 
-        // drilling down to the needed data and place in a variable
+    // drilling down to the needed data and place in a variable
             var refinedResults = data.data.results[0];
             console.log(refinedResults); // console log of comic result
             console.log("-----------------------------------------------------------------------------------------------------------------------------");
             console.log("-----------------------------------------------------------------------------------------------------------------------------");
 
-        // Useful results from refinded results
+    // Useful results from refinded results
             // 1. comics --  its an object
             // 2. description -- description of the comic               
             // 3. events --  its an object
             // 4. name -- name of the comic
             // 5. series --  its an object
             // 6. stories --  its an object
+            // 7. thumbnail  --  its an object
 
-        // store the refined results in variables
+
+    // store the refined results in variables
             var comicResults = data.data.results[0].comics.items; // comics object
             var descriptionResults = data.data.results[0].description; // description item
             var eventsResults = data.data.results[0].events.items; // events object
             var nameResults = data.data.results[0].name; // name item
             var seriesResults = data.data.results[0].series.items; // series object
             var storiesResults = data.data.results[0].stories.items; // stories object
+            var thumbnailResults = data.data.results[0].thumbnail.path; // thumbnail item
 
             console.log("List of comics");
             console.log(comicResults); // console log of comic result
@@ -59,11 +60,29 @@ $( document ).ready(function() {
             console.log("-----------------------------------------------------------------------------------------------------------------------------");
             console.log("List of stories");
             console.log(storiesResults); // console log of stories result
+            console.log("-----------------------------------------------------------------------------------------------------------------------------");
+            console.log("Thumbnail");
+            console.log(thumbnailResults); // console log of thumbnail result
 
 // -----------------------------------------------------------------------------------------------------------------------------------------------------
-            // var jsonText = JSON.stringify(comicResults);
-            // $("#comicsView").html(jsonText);
-                         
+            
+            var jsonTextComics = JSON.stringify(comicResults);
+            $("#comicsView").append(jsonTextComics);
+            $("#comicsView").append("</br></br></br>");
+            var jsonTextDescription = JSON.stringify(descriptionResults);
+            $("#comicsView").append(jsonTextDescription);
+            $("#comicsView").append("</br></br></br>");
+            var jsonTextEvents = JSON.stringify(eventsResults);
+            $("#comicsView").append(jsonTextEvents);
+            $("#comicsView").append("</br></br></br>");
+            var jsonTextName = JSON.stringify(nameResults);
+            $("#comicsView").append(jsonTextName);
+            $("#comicsView").append("</br></br></br>");
+            var jsonTextSeries = JSON.stringify(seriesResults);
+            $("#comicsView").append(jsonTextSeries);
+            $("#comicsView").append("</br></br></br>");
+            var jsonTextStories = JSON.stringify(storiesResults);
+            $("#comicsView").append(jsonTextStories);
          });
 
         return false; // stop the page from refreshing when the submit button is hit
